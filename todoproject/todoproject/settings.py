593823dfccdba541a83261d0b1efca8cfdbfcea4
muringayianne05-prog <muringayianne05@gmail.com>
2026-04-30 15:02:22 +0200

    Set DEBUG=False by default for production

--- todoproject/todoproject/settings.py
+++ todoproject/todoproject/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-ni7ehx&s7%a9_fiuzov(^(yah*p0o=so*f4bt@87yxc1wt(x_x')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=True, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config(
     'ALLOWED_HOSTS',
